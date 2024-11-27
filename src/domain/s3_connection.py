c01f735d89ba81b59840863306b92d08c0feb04c
Galimianov Matvey <mat1073m@gmail.com>
2024-11-28 01:10:06 +0300

    add response status

---
+++ src/domain/s3_connection.py
@@ -0,0 +1,61 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+from aiobotocore.session import get_session
+from botocore.exceptions import ClientError
+
+
+class S3Client:
+    def __init__(
+            self,
+            access_key: str,
+            secret_key: str,
+            endpoint_url: str,
+            bucket_name: str,
+    ):
+        self.config = {
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key": secret_key,
+            "endpoint_url": endpoint_url,
+        }
+        self.bucket_name = bucket_name
+        self.session = get_session()
+
+    @asynccontextmanager
+    async def get_client(self):
+        async with self.session.create_client("s3", **self.config) as client:
+            yield client
+
+    async def upload_file(
+            self,
+            url: str,
+    ) -> str | None:
+        object_name = url.split("/")[-1]
+        try:
+            async with self.get_client() as client:
+                with open(url, "rb") as file:
+                    await client.put_object(
+                        Bucket=self.bucket_name,
+                        Key=object_name,
+                        Body=file,
+                    )
+                    return url
+        except ClientError as e:
+            return None
+
+    async def delete_file(self, object_name: str) -> bool | None:
+        try:
+            async with self.get_client() as client:
+                await client.delete_object(Bucket=self.bucket_name, Key=object_name)
+                return True
+        except ClientError as e:
+            return None
+
+    async def get_file(self, object_name: str) -> None: #что возвращать??
+        try:
+            async with self.get_client() as client:
+                response = await client.get_object(Bucket=self.bucket_name, Key=object_name)
+                data = await response["Body"].read()
+                return data
+        except ClientError as e:
+            return None
