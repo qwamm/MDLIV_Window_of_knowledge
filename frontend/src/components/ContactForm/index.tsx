a387c86eb82493b225800fff82ecbc2eac407aa5
teg0 <abc@mail.ru>
2024-11-27 12:35:52 +0300

    frontend updated

---
+++ frontend/src/components/ContactForm/index.tsx
@@ -0,0 +1,28 @@
+import { Row, Col } from "antd";
+import { withTranslation } from "react-i18next";
+import { Slide } from "react-awesome-reveal";
+import { ContactProps, ValidationTypeProps } from "./types";
+import { useForm } from "../../common/utils/useForm";
+import validate from "../../common/utils/validationRules";
+import { Button } from "../../common/Button";
+import Block from "../Block";
+import Input from "../../common/Input";
+import TextArea from "../../common/TextArea";
+import { ContactContainer, FormGroup, Span, ButtonContainer } from "./styles";
+
+const Contact = ({ title, content, id, t }: ContactProps) => {
+  const { values, errors, handleChange, handleSubmit } = useForm(validate);
+
+  const ValidationType = ({ type }: ValidationTypeProps) => {
+    const ErrorMessage = errors[type as keyof typeof errors];
+    return <Span>{ErrorMessage}</Span>;
+  };
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default withTranslation()(Contact);
